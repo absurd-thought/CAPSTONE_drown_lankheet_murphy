769ff09b93a247e2df8aa5e2846062e195a3043f
Pkeet99 <plankhee@umich.edu>
2023-03-11 14:13:33 -0500

    Updated SQL transformations with new tables.
    Updated SQL transformations with new tables.

---
+++ .ipynb_checkpoints/secretsPaul-checkpoint.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# secrets.py
+secrets = {
+    'DATABASE_ENDPOINT': 'drown-murphy-lankheet.cluster-ceswqg9qwa9i.us-east-1.rds.amazonaws.com',
+    'DATABASE_USER': 'admin',
+    'DATABASE_PASSWORD': 'Capstone699',
+    'DATABASE_PORT': 3306
+}
+
