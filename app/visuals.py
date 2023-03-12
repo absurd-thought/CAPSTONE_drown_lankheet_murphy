c5cb57620ceded8988cc7de5686ba3f037d93fae
Gretchyn Drown <71278574+absurd-thought@users.noreply.github.com>
2023-03-12 14:07:12 -0700

    Update visuals.py

--- app/visuals.py
+++ app/visuals.py
@@ -6,9 +6,6 @@ def get_amenities_visual(amenities_df):#, amen):
     import pandas as pd
     import altair as alt
 
-    # can_add = [ame for ame in amenities_df.index if ame not in amen]
-    # add_df = amenities_df.loc[amenities_df.index.isin(can_add), :]
-    # add_df = add_df.reset_index()
     amenities_df = amenities_df.reset_index()
 
     chart = alt.Chart(amenities_df).mark_rect().encode(
@@ -24,23 +21,22 @@ def get_amenities_visual(amenities_df):#, amen):
 ################################################################################################################
 def get_review_wordcloud(terms_df):
     '''
-    Uses the terms_df to make a word cloud of top 5 review terms
+    Uses the terms_df to make a word cloud of top 10 review terms
     '''
     import pandas as pd
     from wordcloud import WordCloud
     import matplotlib.pyplot as plt
 
-    terms_dict = terms_df.set_index('word')['TF-IDF'].to_dict()
+    terms_dict = terms_df.set_index('term')['freq'].to_dict()
 
     # adapted from https://stackoverflow.com/questions/61916096/word-cloud-built-out-of-tf-idf-vectorizer-function
-    cloud = WordCloud(background_color="#0e1117", max_words=10,
+    cloud = WordCloud(background_color='#0e1117', max_words=10,
                       margin=0, colormap='winter').generate_from_frequencies(terms_dict)
 
     # adapted from https://towardsdatascience.com/simple-wordcloud-in-python-2ae54a9f58e5
     def plot_cloud(wordcloud):
-        plt.figure(figsize=(6, 4), facecolor='k')
+        plt.figure(figsize=(6, 4))
         plt.imshow(wordcloud)
         plt.axis("off")
         plt.tight_layout(pad=0);
-
     return plot_cloud(cloud)
