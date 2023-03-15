064c35bdead3da7f4e87d8b0c2c2a1f835c8c622
Gretchyn Drown <71278574+absurd-thought@users.noreply.github.com>
2023-03-14 17:13:17 -0700

    fixed tf-idf function in nlp.py

--- app/nlp.py
+++ app/nlp.py
@@ -57,13 +57,12 @@ def get_top_amenities(listing_df):
 ##############################################################################################################
 def get_top_review_terms(reviews_df, hostnames):
     '''
-    returns df of top 10 review terms by frequency
+    returns df of top 10 positive and negative review terms by tf-idf
     '''
     import pandas as pd
     import nltk
-    from collections import Counter
-    nltk.download('stopwords', quiet=True)
-    nltk.download('wordnet', quiet=True)
+    nltk.download('stopwords', quiet=True)	
+    nltk.download('wordnet', quiet=True)	
     nltk.download('omw-1.4', quiet=True)
     from nltk.corpus import stopwords
     from nltk.tokenize import RegexpTokenizer
@@ -73,8 +72,19 @@ def get_top_review_terms(reviews_df, hostnames):
     tokenizer = RegexpTokenizer(r'\w+')
     stop_words = set(stopwords.words('english'))
     lemmatizer = WordNetLemmatizer()
+    tfIdfVectorizer = TfidfVectorizer()
     sid = SentimentIntensityAnalyzer()
     punc = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'  # from string.punctuation...removed apostrophe
+
+    # adding to stop words
+    more_stop_words = ['great', 'nice', 'definitely', 'well', 'good', 'loved', 'enjoyed', 'like', 'wonderful', 'thank',
+                       'sure', 'thanks', 'thank', 'love', 'better', 'want', 'cool', 'hope', 'truly', 'appreciate',
+                       'wish', 'okay',
+                       'save', 'yes', 'no', 'share', 'number', 'br', 's', 'u', 'l', 'ok', 'amaze', 'excellent',
+                       'fantastic',
+                       'awesome', 'friend', 'kind', 'help']
+    for word in more_stop_words:
+        stop_words.add(word)
 
     # tokenizing
     tokens = []
@@ -82,40 +92,88 @@ def get_top_review_terms(reviews_df, hostnames):
         comment = tokenizer.tokenize(comment.lower().strip().strip(punc))
         tokens.append(comment)
 
-    # removing stopwords
-    stop_words.add('br')  # breaks in lines show up as 'br'
-    stop_words.add('s')  # 's' is a result of possessives
-
-    new_tokens = []
-    for item in tokens:
-        new = [i for i in item if not i in stop_words]
-        new_no_digits = [i for i in new if not i.isdigit()]  # removing numerals
-        new_no_hosts = [i for i in new_no_digits if not i in hostnames]  # removing hostnames
-        new_tokens.append(new_no_hosts)
-
     # lemmatizing
     lemms = []
-    for item in new_tokens:
-        l = []
-        for word in item:
-            lemm = lemmatizer.lemmatize(word)
-            l.append(lemm)
+    for item in tokens:
+        l = [lemmatizer.lemmatize(word, pos='v') for word in item]
         lemms.append(l)
 
-    # getting only positive sentiments
-    pos_tokens = []
-    for each_list in lemms:
-        tok2 = []
+    new_tokens = []
+    for item in lemms:
+        new = [i for i in item if not i in stop_words]  # removing stop words
+        new_no_digits = [i for i in new if not i.isdigit()]  # removing numerals
+        new_no_hosts = [i for i in new_no_digits if not i in hostnames2]  # removing hostnames
+        new_tokens.append(new_no_hosts)
+
+    # getting positive and negative sentiments
+    pos_lemms = []
+    neg_lemms = []
+    for each_list in new_tokens:
+        pos = []
+        neg = []
         for each_word in each_list:
-            if sid.polarity_scores(each_word)['compound'] >= 0.5 or sid.polarity_scores(each_word)['pos'] == 1.0:
-                tok2.append(each_word)
-        pos_tokens.append(tok2)
+            if sid.polarity_scores(each_word)['pos'] == 1.0:
+                pos.append(each_word)
+            elif sid.polarity_scores(each_word)['neg'] == 1.0:
+                neg.append(each_word)
+        pos_lemms.append(pos)
+        neg_lemms.append(neg)
 
-    flat_list_corpus = [item for sublist in pos_tokens for item in sublist]
-    c = Counter(flat_list_corpus)
-    data = c.most_common(10)
-    df = pd.DataFrame.from_records(data, columns=['term', 'freq'])
+    # creating dataframe of positive and negative terms
+    lemms_df = pd.DataFrame({'pos_lemms': pos_lemms, 'neg_lemms': neg_lemms})
+    lemms_df['pos_lemms'] = lemms_df['pos_lemms'].astype(str)
+    lemms_df['neg_lemms'] = lemms_df['neg_lemms'].astype(str)
 
-    return df
+    # performing tf-idf
+    # adapted from https://stackoverflow.com/questions/45805493/sorting-tfidfvectorizer-output-by-tf-idf-lowest-to-highest-and-vice-versa
+
+    # getting corpi
+    pos_corpus = lemms_df['pos_lemms']
+    neg_corpus = lemms_df['neg_lemms']
+
+    ## POSITIVE ##
+    # fitting model
+    tfIdf = tfIdfVectorizer.fit_transform(pos_corpus)
+
+    # getting vocabulary
+    terms = tfIdfVectorizer.get_feature_names_out()
+
+    # finding highest ranked words
+    sums = tfIdf.sum(axis=0)
+    pos_data = {}
+    for col, term in enumerate(terms):
+        pos_data[term] = sums[0, col]
+
+    pos_data_sorted = sorted(pos_data, key=pos_data.get, reverse=True)
+
+    pos_ranked = {}
+    for r in pos_data_sorted:
+        pos_ranked[r] = pos_data[r]
+    pos_ranked_df = pd.DataFrame.from_dict(pos_ranked, orient='index').reset_index()
+    pos_ranked_df = pos_ranked_df.rename(columns={'index': 'pos_word', 0: 'pos_score'})
+
+    ## NEGATIVE ##
+    # fitting model
+    tfIdf = tfIdfVectorizer.fit_transform(neg_corpus)
+
+    # getting vocabulary
+    terms = tfIdfVectorizer.get_feature_names_out()
+
+    # finding highest ranked words
+    sums = tfIdf.sum(axis=0)
+    neg_data = {}
+    for col, term in enumerate(terms):
+        neg_data[term] = sums[0, col]
+    neg_data_sorted = sorted(neg_data, key=neg_data.get, reverse=True)
+
+    neg_ranked = {}
+    for r in neg_data_sorted:
+        neg_ranked[r] = neg_data[r]
+    neg_ranked_df = pd.DataFrame.from_dict(neg_ranked, orient='index').reset_index()
+    neg_ranked_df = neg_ranked_df.rename(columns={'index': 'neg_word', 0: 'neg_score'})
+
+    result = pd.concat([pos_ranked_df, neg_ranked_df], axis=1, join='inner')
+
+    return result[:10]
 
 ##############################################################################################################
