00c48ffe55de684b90d0e5e5f77aed6f51d2327b
Gretchyn Drown <71278574+absurd-thought@users.noreply.github.com>
2023-03-14 21:13:07 -0700

    Update nlp.py

--- app/nlp.py
+++ app/nlp.py
@@ -67,6 +67,7 @@ def get_top_review_terms(reviews_df, hostnames):
     from nltk.corpus import stopwords
     from nltk.tokenize import RegexpTokenizer
     from nltk.stem import WordNetLemmatizer
+    from sklearn.feature_extraction.text import TfidfVectorizer
     from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
     tokenizer = RegexpTokenizer(r'\w+')
