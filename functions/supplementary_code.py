468559ff46a234e09e549f861c0698bfc041ad0e
Gretchyn Drown <71278574+absurd-thought@users.noreply.github.com>
2023-04-14 15:24:09 -0700

    Update supplementary_code.py

--- functions/supplementary_code.py
+++ functions/supplementary_code.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
 
 import pandas as pd
 import pymysql
@@ -15,14 +13,13 @@ port = secrets.get('DATABASE_PORT')
 database = secrets.get('DATABASE_NAME')
 
 engine = create_engine(f'mysql+pymysql://{user}:{password}@{endpoint}:{port}/{database}', pool_recycle=3600);
-
-
-################## Choropleth map with count of unique listings by state ##################################################
+############################################################################################################
+# Choropleth map with count of unique listings by state
 query = f'SELECT scrape_city, listing_id FROM {database}.listings;'
 all_city_listings = pd.read_sql_query(query, engine)
 
 areas_dict= {'NC':['---', 'asheville'], 'FL':['---', 'broward-county'], 'MA':['---', 'boston', 'cambridge'], 'HI':['---', 'hawaii'],
-             'IL':['---', 'chicago'], 'NV':['---', 'clark-county-nv'], 'OH': ['---', 'columbus'], 'CO':['---', 'denver'],
+             'IL':['---', 'chicago'], 'NV':['---', 'clark-county'], 'OH': ['---', 'columbus'], 'CO':['---', 'denver'],
              'TX':['---', 'austin', 'dallas', 'fort-worth'], 'TN':['---', 'nashville'], 'LA':['---', 'new-orleans'],
              'NY':['---', 'new-york-city'], 'NJ':['---', 'jersey-city', 'newark'], 'MN':['---', 'twin-cities-msa'],
              'RI':['---', 'rhode-island'],  'OR':['---', 'portland', 'salem-or'],
@@ -49,8 +46,39 @@ def get_listings_by_state():
                     locationmode="USA-states", color='unique_listings', scope="usa",color_continuous_scale='teal')
  
     fig.show()
-##################################################################################################
+##################################################################################################################
+# Horizontal bar chart showing Reviews sentiments percents (Example: Boston, MA)
+query = f"SELECT comments FROM {database}.reviews WHERE scrape_city='boston'"
+reviews = pd.read_sql_query(query, engine)
 
+def get_reviews_sentiments(reviews_df, full_area_name):
+    '''
+    full_area_name should be a string with city, state. Example: 'Boston, MA'
+    '''
+    import pandas as pd
+    from collections import Counter
 
+    scores_dict_list = []
+    for each_list in reviews_df['comments']:
+        scores_dict_list.append(sid.polarity_scores(each_list))
 
+    counter = Counter()
+    for d in scores_dict_list:
+        counter.update(d)
 
+    result = dict(counter)
+    source = pd.DataFrame.from_dict(result, orient='index').reset_index()
+    source = source.rename(columns={'index': 'sentiment', 0: 'value'})
+
+    chart = alt.Chart(source, title=f'Review Sentiments for {full_area_name}').transform_joinaggregate(
+        TotalValue='sum(value)',
+    ).transform_calculate(
+        PercentOfTotal="datum.value / datum.TotalValue"
+    ).mark_bar(color='#057775').encode(
+        alt.X('PercentOfTotal:Q', axis=alt.Axis(format='.0%')),
+        y='sentiment:N'
+    )
+
+    return chart
+
+get_reviews_sentiments(reviews, 'Boston, MA')
