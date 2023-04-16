05a81a0535d9e84c922dba86e21cc2e4e9dbb9ca
Gretchyn Drown <71278574+absurd-thought@users.noreply.github.com>
2023-04-15 18:24:29 -0700

    Update About.py

--- app/About.py
+++ app/About.py
@@ -16,13 +16,13 @@ st.title(":blue[And That Means Comfort: Optimizing Airbnb Listings]")
 
 st.markdown(
     """
-    ### An Airbnb listing optimizer for hosts.
+    ### A dynamic Airbnb tool for hosts.
     Find out which amenities add the most perceived value to your home, explore terms to include in your description, and predict your best price!
     
     **👈 Select a feature from the sidebar** to help create your listing
     
     ### Want to learn more?
-    - Check out the full report of [our blog post](https://streamlit.io)
-    - Create your own web app like this with our [GitHub](https://docs.streamlit.io)
+    - Check out the full report of how we did it at [our blog post](https://medium.com/@gdrown/and-that-means-comfort-optimizing-airbnb-listings-fbc0ce1e052d)
+    - Create your own Airbnb web app like this with our [GitHub](https://github.com/absurd-thought/CAPSTONE_drown_lankheet_murphy)
 """
 )
